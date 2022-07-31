9f99c22ec71782afe7bd53484fe9c138aedebfc3
painnick <painnick@gmail.com>
2022-07-31 18:36:09 +0900

    ADD. Right Arm 및 Gun LED 추가

--- nodemcu_sample1/nodemcu_sample1.ino
+++ nodemcu_sample1/nodemcu_sample1.ino
@@ -1,35 +1,63 @@
 #include "analogWrite.h"
 #include <ESP32Servo.h>
+#include "soc/soc.h"             // disable brownout problems
+#include "soc/rtc_cntl_reg.h"    // disable brownout problems
 
-#define PIN1 32
-#define PIN2 33
-#define PIN3 25
-#define PIN4 26
+#define PIN_TRACK_A1 32
+#define PIN_TRACK_A2 33
+#define PIN_TRACK_B1 25
+#define PIN_TRACK_B2 26
 
-#define PIN_WAIST 13
-#define PIN_LEFT_ARM 23
+#define PIN_WAIST 21
+#define PIN_LEFT_ARM 19
+#define PIN_RIGHT_ARM 18
+
+#define PINT_LEFT_GUN 5
+#define PINT_RIGHT_GUN 17
 
 Servo waist;
-Servo leftgArm;
+Servo leftArm, rightArm;
 
 void setup() {
 
+  WRITE_PERI_REG(RTC_CNTL_BROWN_OUT_REG, 0); //disable brownout detector
+
+  Serial.begin(115200);
+  // Serial.setDebugOutput(false);
+
   waist.setPeriodHertz(50);    // standard 50 hz servo
   waist.attach(PIN_WAIST, 1000, 2000);
+  waist.write(0);
+  delay(500);
 
-  leftgArm.setPeriodHertz(50);    // standard 50 hz servo
-  leftgArm.attach(PIN_LEFT_ARM, 1000, 2000);
+  leftArm.setPeriodHertz(50);    // standard 50 hz servo
+  leftArm.attach(PIN_LEFT_ARM, 1000, 2000);
+  leftArm.write(0);
+  delay(500);
 
+  rightArm.setPeriodHertz(50);    // standard 50 hz servo
+  rightArm.attach(PIN_LEFT_ARM, 1000, 2000);
+  rightArm.write(0);
+  delay(500);
 
-  pinMode(PIN1, OUTPUT);
-  pinMode(PIN2, OUTPUT);
-  pinMode(PIN3, OUTPUT);
-  pinMode(PIN4, OUTPUT);
+  pinMode(PIN_TRACK_A1, OUTPUT);
+  pinMode(PIN_TRACK_A2, OUTPUT);
+  pinMode(PIN_TRACK_B1, OUTPUT);
+  pinMode(PIN_TRACK_B2, OUTPUT);
 
-  waist.write(90);
-  leftgArm.write(90);
+  pinMode(PINT_LEFT_GUN, OUTPUT);
+  pinMode(PINT_RIGHT_GUN, OUTPUT);
 
-  delay(1000 * 10);
+  Serial.println("Setup.(1/5)");
+  delay(1000);
+  Serial.println("Setup..(2/5)");
+  delay(1000);
+  Serial.println("Setup...(3/5)");
+  delay(1000);
+  Serial.println("Setup....(4/5)");
+  delay(1000);
+  Serial.println("Setup.....(5/5)");
+  delay(1000);
 }
 
 bool forward = true;
@@ -37,18 +65,24 @@ int forwardIndex = 0;
 
 int direction = 0;
 int leftArmRadius = 0;
+int rightArmRadius = 0;
 
 void loop() {
+
+  Serial.println("===== Loop start =====");
+
   if (forward) {
-    analogWrite(PIN1, 255);
-    analogWrite(PIN2, 0);
-    analogWrite(PIN3, 255);
-    analogWrite(PIN4, 0);
+    digitalWrite(PIN_TRACK_A1, HIGH);
+    digitalWrite(PIN_TRACK_A2, LOW);
+    digitalWrite(PIN_TRACK_B1, HIGH);
+    digitalWrite(PIN_TRACK_B2, LOW);
+    Serial.println("Track set Forward");
   } else {
-    analogWrite(PIN1, 0);
-    analogWrite(PIN2, 255);
-    analogWrite(PIN3, 0);
-    analogWrite(PIN4, 255);
+    digitalWrite(PIN_TRACK_A1, LOW);
+    digitalWrite(PIN_TRACK_A2, HIGH);
+    digitalWrite(PIN_TRACK_B1, LOW);
+    digitalWrite(PIN_TRACK_B2, HIGH);
+    Serial.println("Track set Backward");
   }
 
   delay(1000);
@@ -58,11 +92,40 @@ void loop() {
     forward = !forward;
   }
 
-  direction = (direction + 10) % 180 + 90;
+  direction = (direction + 10) % 180;
   waist.write(direction);
+  Serial.print("Waist set ");
+  Serial.println(direction);
+
   delay(500);
 
-  leftArmRadius = (leftArmRadius + 15) % 45 + 90;
-  leftgArm.write(leftArmRadius);
+  leftArmRadius = (leftArmRadius + 15) % 45;
+  leftArm.write(leftArmRadius);
+  Serial.print("Left arm set");
+  Serial.println(leftArmRadius);
+
   delay(500);
+
+  if (leftArmRadius < 15) {
+    analogWrite(PINT_LEFT_GUN, 255);
+    Serial.println("Left gun set On");
+  } else {
+    analogWrite(PINT_LEFT_GUN, 0);
+    Serial.println("Left gun set Off");
+  }
+
+  rightArmRadius = (rightArmRadius + 10) % 60;
+  rightArm.write(rightArmRadius);
+  Serial.print("Right arm set ");
+  Serial.println(rightArmRadius);
+
+  delay(500);
+
+  if (rightArmRadius < 10) {
+    analogWrite(PINT_RIGHT_GUN, 255);
+    Serial.println("Right gun set On");
+  } else {
+    analogWrite(PINT_RIGHT_GUN, 0);
+    Serial.println("Right gun set Off");
+  }
 }
